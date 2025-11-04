a80136b2d9da0b1d9fbf8a62551f364ac64af1d9
Loran Saggu <loransaggu@hotmail.com>
2025-11-04 15:51:57 +1100

    adjusting after testing responses

--- src/modules/main/weather/weather.js
+++ src/modules/main/weather/weather.js
@@ -20,11 +20,13 @@ export default class Weather extends LightningElement {
         const apiKey = 'your-api-key';  // NOTE: This is an insecure pattern that publicly exposes your API Key
                                         // A safer pattern is to call a backend service that can call the API using the private credentials
                                         // The backend service should employ CORS to restrict access to approved consumers
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apiKey}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apiKey}&units=metric`;
         const response = await fetch(url);
         const data = await response.json();
-        this.temperature = data.main.temp;
-        this.humidity = data.main.humidity;
-        this.description = data.weather[0].description;
+        console.log(data);
+        this.temperature = data.main.temp + ' C';
+        this.humidity = data.main.humidity + ' %';
+        let desc = data.weather[0].description;
+        this.description = desc.charAt(0).toUpperCase() + desc.slice(1);
     }
 }
