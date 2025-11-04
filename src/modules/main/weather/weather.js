aa2766d78d055a35b5403d0eec36034c39d5116e
Loran Saggu <loransaggu@hotmail.com>
2025-11-04 15:56:26 +1100

    noting where to access api key;

--- src/modules/main/weather/weather.js
+++ src/modules/main/weather/weather.js
@@ -20,6 +20,7 @@ export default class Weather extends LightningElement {
         const apiKey = 'your-api-key';  // NOTE: This is an insecure pattern that publicly exposes your API Key
                                         // A safer pattern is to call a backend service that can call the API using the private credentials
                                         // The backend service should employ CORS to restrict access to approved consumers
+                                        // API Key is available on https://openweathermap.org/
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apiKey}&units=metric`;
         const response = await fetch(url);
         const data = await response.json();
